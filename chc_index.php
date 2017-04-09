b65e4acccf0ba0c343bb6c7bbb2b0ff498a5a352
C0der1iu <356575787@qq.com>
2017-04-09 16:34:26 +0800

    Add files via upload

---
+++ chc_index.php
@@ -0,0 +1,41 @@
+﻿<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+<html>
+<head>
+<title>GUEST BOOK ，HELLO LOSER</title>
+</head>
+<body>
+<br />
+<br />
+<h2>留言板<h2>
+
+<form action="chc_index.php" method="post">
+info:<textarea id='uid' name="desc"></textarea>
+<br>
+<br>
+user:<input type="text" name="user"/><br>
+<input type="submit" value="GO!" onclick='loction="chc_index.php"'/>
+</form>
+<?php
+if(isset($_POST['user'])&&isset($_POST['desc'])){
+$log=fopen("sql.txt","a");
+function convertScript($string)
+{
+return str_replace("<script>", "", $string);
+}
+fwrite($log,filter_var($_POST['user'], FILTER_CALLBACK, array("options"=>"convertScript"))."\r\n");
+fwrite($log,filter_var($_POST['desc'], FILTER_CALLBACK, array("options"=>"convertScript"))."\r\n");
+fclose($log);
+}
+
+if(file_exists("sql.txt"))
+{
+$read= fopen("sql.txt",'r');
+while(!feof($read))
+{
+	echo fgets($read)."</br>";
+}
+fclose($read);
+}
+?>
+</body>
+</html>
---
+++ fs_index.php
@@ -0,0 +1,26 @@
+﻿<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+<html>
+<head>
+<title>GUEST BOOK ，HELLO LOSER</title>
+</head>
+<body>
+<br />
+<br />
+<h2>留言板<h2>
+
+<form action="fs_index.php" method="get">
+info:<textarea id='uid' name="desc"></textarea><br>
+<br>
+user:<input type="text" name="user"/><br>
+<input type="submit" value="GO!"/>
+</form>
+<?php
+if(isset($_GET['user'])&&isset($_GET['desc'])){
+$username=$_GET['user'];
+$desc=$_GET['desc'];
+echo 'user:'."\t".$username."</br>";
+echo 'desc:'."\t".$desc."\n";
+}
+?>
+</body>
+</html>
